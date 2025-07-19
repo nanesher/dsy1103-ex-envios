028c88899f63c9766ec5cb188e99142de763dac3
Hernán Vera Cabrera <hernan-fe@hotmail.com>
2025-07-18 23:13:41 -0400

    Api 'Lista'

--- src/main/java/duoc/cl/ecomarket/examen/envios/model/EnvioRequest.java
+++ src/main/java/duoc/cl/ecomarket/examen/envios/model/EnvioRequest.java
@@ -1,6 +1,7 @@
 package duoc.cl.ecomarket.examen.envios.model;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class EnvioRequest {
+    @Schema(description = "Identificador unico del usuario del envio", example = "6")
     private Integer idUsuario;
+    @Schema(description = "Metodo de recepcion valido para la venta", example = "Retiro en tienda")
     private String tipoEntrega;
 
 }
--- src/main/java/duoc/cl/ecomarket/examen/envios/repository/DataLoader.java
+++ src/main/java/duoc/cl/ecomarket/examen/envios/repository/DataLoader.java
@@ -1,4 +1,41 @@
 package duoc.cl.ecomarket.examen.envios.repository;
 
-public class DataLoader {
+import duoc.cl.ecomarket.examen.envios.model.Envio;
+import net.datafaker.Faker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Locale;
+import java.util.Random;
+
+@Component
+@Profile("!test")
+public class DataLoader implements CommandLineRunner {
+    @Autowired
+    private EnvioRepository EnvioRepository;
+    public void run(String... args) throws Exception {
+        Faker faker = new Faker(new Locale("es"));
+        Random random = new Random();
+
+        for (int i = 0; i < 15; i++) {
+            Envio envio = new Envio();
+            envio.setIdUsuario(faker.number().numberBetween(1,15));
+            String[] estados = {"En proceso", "En transito", "Entregado"};
+            String estado = estados[random.nextInt(estados.length)];
+            envio.setEstado(estado);
+            envio.setFechaSolicitud(LocalDateTime.now());
+            envio.setFechaEntrega(LocalDate.now().plusDays(1));
+            String[] tipos = {"Retiro en tienda", "Envio a domicilio"};
+            String tipo = tipos[random.nextInt(tipos.length)];
+            envio.setTipoEntrega(tipo);
+            envio.setNombreCliente(faker.name().fullName());
+            envio.setDireccionEntrega(faker.address().fullAddress());
+            EnvioRepository.save(envio);
+        }
+    }
 }
+
